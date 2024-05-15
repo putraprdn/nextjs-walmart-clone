ee21d26446bdc90595107b5bae48514db6fd820d
putraprdn <80741806+putraprdn@users.noreply.github.com>
2024-05-15 14:23:35 +0700

    initial commit

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,25 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+	images: {
+		remotePatterns: [
+			{
+				protocol: "https",
+				hostname: "links.papareact.com",
+			},
+			{
+				protocol: "https",
+				hostname: "i.imgur.com",
+			},
+			{
+				protocol: "https",
+				hostname: "i5.walmartimages.com",
+			},
+			{
+				protocol: "https",
+				hostname: "content.syndigo.com",
+			},
+		],
+	},
+};
 
 export default nextConfig;
